a98fa3b20edc81c00b416d3916a52823bc386d82
Hyunsung Kim <hyunsunghero@gmail.com>
2022-01-12 02:14:56 +0000

    [refactor] Minor changes in variable names

--- src/rolerep.py
+++ src/rolerep.py
@@ -19,7 +19,7 @@ class RoleRep:
     def __init__(self, ugp_df_):
         self.ugp_df = ugp_df_
         self.fgp_df = None
-        self.role_records = None
+        self.role_distns = None
 
     @staticmethod
     def _normalize_locs(moment_fgp_df):
@@ -63,40 +63,40 @@ class RoleRep:
     @staticmethod
     def update_params(fgp_df, by_phase=False):
         cols = [LABEL_PLAYER_PERIOD, LABEL_ROLE] if by_phase else [LABEL_ROLE]
-        role_records = fgp_df.groupby(cols).apply(RoleRep._estimate_mvn).reset_index()
-        return role_records.dropna().rename(columns={0: LABEL_DISTN})
+        role_distns = fgp_df.groupby(cols).apply(RoleRep._estimate_mvn).reset_index()
+        return role_distns.dropna().rename(columns={0: LABEL_DISTN})
 
     @staticmethod
-    def align_formations(fgp_df, role_records, label_group=LABEL_SESSION):
+    def align_formations(fgp_df, role_distns, label_group=LABEL_SESSION):
         groups = fgp_df[label_group].unique()
-        base_group = groups[role_records.groupby(label_group)[LABEL_ROLE].count().argmax()]
-        base_role_records = role_records[role_records[label_group] == base_group]
+        base_group = groups[role_distns.groupby(label_group)[LABEL_ROLE].count().argmax()]
+        base_role_distns = role_distns[role_distns[label_group] == base_group]
 
         for group in groups:
             if group == base_group:
                 continue
-            group_role_records = role_records[role_records[label_group] == group]
+            group_role_distns = role_distns[role_distns[label_group] == group]
             cost_mat = distance_matrix(
-                group_role_records[LABEL_DISTN].apply(lambda x: pd.Series(x.mean)).values,
-                base_role_records[LABEL_DISTN].apply(lambda x: pd.Series(x.mean)).values
+                group_role_distns[LABEL_DISTN].apply(lambda x: pd.Series(x.mean)).values,
+                base_role_distns[LABEL_DISTN].apply(lambda x: pd.Series(x.mean)).values
             )
             row_idx, col_idx = linear_sum_assignment(cost_mat)
-            role_dict = dict(zip(group_role_records[LABEL_ROLE].iloc[row_idx],
-                                 base_role_records[LABEL_ROLE].iloc[col_idx]))
+            role_dict = dict(zip(group_role_distns[LABEL_ROLE].iloc[row_idx],
+                                 base_role_distns[LABEL_ROLE].iloc[col_idx]))
             role_dict[0] = 0
-            role_records.loc[role_records[label_group] == group, LABEL_ROLE] = col_idx + 1
+            role_distns.loc[role_distns[label_group] == group, LABEL_ROLE] = col_idx + 1
             fgp_df.loc[fgp_df[label_group] == group, LABEL_ROLE] = \
                 fgp_df.loc[fgp_df[label_group] == group, LABEL_ROLE].apply(lambda role: role_dict[role])
             fgp_df.loc[fgp_df[label_group] == group, LABEL_BASE_ROLE] = \
                 fgp_df.loc[fgp_df[label_group] == group, LABEL_BASE_ROLE].apply(lambda role: role_dict[role])
 
-        return fgp_df, role_records.sort_values(by=[label_group, LABEL_ROLE]).reset_index(drop=True)
+        return fgp_df, role_distns.sort_values(by=[label_group, LABEL_ROLE]).reset_index(drop=True)
 
-    def _hungarian(self, moment_fgp_df, role_records):
+    def _hungarian(self, moment_fgp_df, role_distns):
         cost_mat = moment_fgp_df[moment_fgp_df.columns[(len(HEADER_FGP) + 1):]].values
         row_idx, col_idx = linear_sum_assignment(cost_mat)
         base_roles = moment_fgp_df[LABEL_BASE_ROLE].iloc[row_idx].values
-        temp_roles = role_records[LABEL_ROLE].iloc[col_idx].values
+        temp_roles = role_distns[LABEL_ROLE].iloc[col_idx].values
         self.fgp_df.loc[moment_fgp_df.index, LABEL_ROLE] = temp_roles
         self.fgp_df.loc[moment_fgp_df.index, LABEL_SWITCH_RATE] = (base_roles != temp_roles).sum() / len(row_idx)
         return cost_mat[row_idx, col_idx].mean()
@@ -112,10 +112,10 @@ class RoleRep:
     def run(self, freq='1S', verbose=True):
         temp_fgp_df = self.ugp_df.groupby(LABEL_PLAYER_PERIOD).apply(RoleRep._generate_fgp, freq=freq)
         temp_fgp_df = temp_fgp_df.reset_index(drop=True).dropna()
-        temp_role_records = RoleRep.update_params(temp_fgp_df, by_phase=True)
-        temp_fgp_df = pd.merge(temp_fgp_df, temp_role_records[[LABEL_PLAYER_PERIOD, LABEL_ROLE]])
-        self.fgp_df, _ = RoleRep.align_formations(temp_fgp_df, temp_role_records, LABEL_PLAYER_PERIOD)
-        self.role_records = RoleRep.update_params(self.fgp_df)
+        temp_role_distns = RoleRep.update_params(temp_fgp_df, by_phase=True)
+        temp_fgp_df = pd.merge(temp_fgp_df, temp_role_distns[[LABEL_PLAYER_PERIOD, LABEL_ROLE]])
+        self.fgp_df, _ = RoleRep.align_formations(temp_fgp_df, temp_role_distns, LABEL_PLAYER_PERIOD)
+        self.role_distns = RoleRep.update_params(self.fgp_df)
 
         max_iter = 10
         cost_prev = float('inf')
@@ -123,15 +123,15 @@ class RoleRep:
         self.fgp_df.reset_index(drop=True, inplace=True)
 
         for i_iter in range(max_iter):
-            cost_df = pd.DataFrame(self.role_records[LABEL_DISTN].apply(
+            cost_df = pd.DataFrame(self.role_distns[LABEL_DISTN].apply(
                 lambda n: pd.Series(-np.log(n.pdf(self.fgp_df[[LABEL_X_NORM, LABEL_Y_NORM]])))
             ).transpose().values, index=self.fgp_df.index)
             fgp_cost_df = pd.concat([self.fgp_df, cost_df], axis=1)
-            costs = fgp_cost_df.groupby(LABEL_DATETIME).apply(self._hungarian, self.role_records).mean()
+            costs = fgp_cost_df.groupby(LABEL_DATETIME).apply(self._hungarian, self.role_distns).mean()
             cost_new = costs.mean()
             if verbose:
                 print('- Cost after iteration {0}: {1:.3f}'.format(i_iter + 1, cost_new))
-            self.role_records = self.update_params(self.fgp_df)
+            self.role_distns = self.update_params(self.fgp_df)
             if cost_new + tol > cost_prev:
                 if verbose:
                     print('Iteration finished since there are no significant changes.')
@@ -139,7 +139,7 @@ class RoleRep:
             cost_prev = cost_new
 
         session = self.ugp_df[LABEL_SESSION].iloc[0]
-        self.role_records[LABEL_SESSION] = session
+        self.role_distns[LABEL_SESSION] = session
 
 
 # if __name__ == '__main__':
@@ -168,7 +168,7 @@ class RoleRep:
 #             match.player_periods[LABEL_ROLE_PERIOD] = match.player_periods[LABEL_SESSION]
 #             match.ugp_df[LABEL_FORM_PERIOD] = match.ugp_df[LABEL_SESSION]
 #             match.ugp_df[LABEL_ROLE_PERIOD] = match.ugp_df[LABEL_SESSION]
-#             match_role_records = pd.DataFrame(columns=HEADER_ROLE_RECORDS)
+#             match_role_distns = pd.DataFrame(columns=HEADER_ROLE_RECORDS)
 #
 #             match_fgp_df = pd.DataFrame(columns=[LABEL_DATETIME] + HEADER_FGP)
 #             for j in match.ugp_df[LABEL_ROLE_PERIOD].unique():
@@ -176,7 +176,7 @@ class RoleRep:
 #                 rolerep = RoleRep(form_ugp_df)
 #                 print(f'\nRunning RoleRep for session {j}...')
 #                 rolerep.run(freq='1S')
-#                 match_role_records = match_role_records.append(rolerep.role_records, sort=True)
+#                 match_role_distns = match_role_distns.append(rolerep.role_distns, sort=True)
 #                 match_fgp_df = match_fgp_df.append(rolerep.fgp_df)
 #
 #             match_fgp_path = f'{DIR_DATA}/fgp_avg/{activity_id}.csv'
@@ -187,10 +187,10 @@ class RoleRep:
 #             print('Not enough players to estimate a formation.')
 #             continue
 #
-#         match_role_records[LABEL_ACTIVITY_ID] = activity_id
-#         match_role_records = match_role_records[[LABEL_ACTIVITY_ID] + HEADER_ROLE_RECORDS]
+#         match_role_distns[LABEL_ACTIVITY_ID] = activity_id
+#         match_role_distns = match_role_distns[[LABEL_ACTIVITY_ID] + HEADER_ROLE_RECORDS]
 #         print()
-#         print(match_role_records)
+#         print(match_role_distns)
 #
 #         # rm.activity_records.at[i, LABEL_STATS_SAVED] = 1
 #         # rm.save_records(VARNAME_ACTIVITY_RECORDS)
